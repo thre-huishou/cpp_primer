b22c6a417700dae19e98c5043ee03139e81b8d94
thre-huishou <2721061625@qq.com>
2021-11-19 16:01:03 +0800

    day2

---
+++ 语法测试/Project1/Project2/111.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+
+//局部域测试，声明从内而外的查找
+//int main()
+//{
+//	for (int i = 0; i < 10; i++)
+//	{
+//		for (int i = 0; i < 10; i++)
+//		{
+//			cout << i << " ";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
